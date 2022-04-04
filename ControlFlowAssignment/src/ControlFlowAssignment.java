63e2dcc72e6cdce403fb93e2e6033e4c3a2fe633
afrikacollier <101090240+afrikacollier@users.noreply.github.com>
2022-04-04 00:22:18 -0700

    changed the variable names to be more descriptive
    added checks for integer inputs as well

--- ControlFlowAssignment/src/ControlFlowAssignment.java
+++ ControlFlowAssignment/src/ControlFlowAssignment.java
@@ -48,34 +48,34 @@ public class ControlFlowAssignment {
 						   * of numbers.  Note that it may be helpful to put example responses 
 						   * after each question.  */
 						  System.out.println("Do you have a red car? (yes, no): ");
-						  String someStringThree = scanner.nextLine();
+						  String haveRedCar = scanner.nextLine();
 						  System.out.println("What is your favorite pet? "); 
-						  String someStringFour = scanner.nextLine(); //let's try isEmpty instead
+						  String favoritePet = scanner.nextLine(); //let's try isEmpty instead
 						  System.out.println("How old is your favorite pet?");
-						  int someStringFive;
+						  int ageOfPet;
 						  do {
 							  System.out.println("Please enter a positive number.");
 							  while (!scanner.hasNextInt()) {
 								  System.out.println("That's not a number.");
 								  scanner.next();
 							  }
-						  someStringFive = scanner.nextInt();  
-						  } while(someStringFive<=0);
+						  ageOfPet = scanner.nextInt();  
+						  } while(ageOfPet<=0);
 						  System.out.println("What is their lucky number? ");
-						  int someStringSix;
+						  int luckyNumber;
 						  do {
 							  System.out.println("Please enter a positive number.");
 							  while(!scanner.hasNextInt()) {
 								  System.out.println("That's not a number.");
 								  scanner.next();
 							  }
-						  someStringSix = scanner.nextInt();
-						  } while (someStringSix <= 0);
+						  luckyNumber = scanner.nextInt();
+						  } while (luckyNumber <= 0);
 						  scanner.nextLine();
 						  System.out.println("Do you have a favorite quarterback? ");
-						  String someStringSeven = scanner.nextLine();
-						  int someStringEight=0;//we need it accessible in outside scope ~IMPORTANT TO NOTE
-						  if (someStringSeven.equals(s) || someStringSeven.equals(b)) {
+						  String favoriteQuarterback = scanner.nextLine();
+						  int jerseyNumber=0;//we need it accessible in outside scope ~IMPORTANT TO NOTE
+						  if (favoriteQuarterback.equals(s) || favoriteQuarterback.equals(b)) {
 							  System.out.println("If so what is their jersey number? ");
 							  do {
 								  System.out.println("Please enter a positive number.");
@@ -83,32 +83,32 @@ public class ControlFlowAssignment {
 									  System.out.println("That's not a number.");
 									  scanner.next();
 								  }
-							  someStringEight = scanner.nextInt();
-							  } while (someStringEight <= 0);
+							  jerseyNumber = scanner.nextInt();
+							  } while (jerseyNumber <= 0);
 						  }
 						  System.out.println("What is the two-digit model year of your car? ");
-						  int someStringNine;
+						  int twoDigitModelYear;
 						  do {
 							  System.out.println("Please enter a positive number.");
 							  while(!scanner.hasNextInt()) {
 								  System.out.println("That's not a number.");
 								  scanner.next();
 							  }
-						  someStringNine = scanner.nextInt();
-						  } while (someStringNine <= 0);
+						  twoDigitModelYear = scanner.nextInt();
+						  } while (twoDigitModelYear <= 0);
 						  scanner.nextLine();
 						  System.out.println("What is the first name of your favorite actor or actress? ");
-						  String someStringTen = scanner.nextLine();
+						  String favoriteActorOrActress = scanner.nextLine();
 						  System.out.println("Enter a number between 1 and 50: ");
-						  int someStringEleven;
+						  int enterNumberBetweenOneAndFifty;
 						  do {
 							  System.out.println("Please enter a positive number.");
 							  while(!scanner.hasNextInt()) {
 								  System.out.println("That's not a number.");
 								  scanner.next();
 							  }
-						  someStringEleven = scanner.nextInt();
-						  } while (someStringEleven <= 0);
+						  enterNumberBetweenOneAndFifty = scanner.nextInt();
+						  } while (enterNumberBetweenOneAndFifty <= 0);
 						  scanner.nextLine();
 						  Random rand = new Random();
 						  int anArray[] = new int[3];
@@ -116,16 +116,16 @@ public class ControlFlowAssignment {
 							  anArray[i] = rand.nextInt(65)+1; //0 -> 1 and 64 -> 65
 						  }
 						  int PowerBallArray[] = new int[7];
-						  PowerBallArray[0] = Math.abs((someStringEight-anArray[1])%66+1);
-						  PowerBallArray[1] = someStringTen.charAt(0)%66+1;
-						  PowerBallArray[2] = (someStringNine + someStringSix)%66+1;
+						  PowerBallArray[0] = Math.abs((jerseyNumber-anArray[1]))%66+1;
+						  PowerBallArray[1] = favoriteActorOrActress.charAt(0)%66+1;
+						  PowerBallArray[2] = (twoDigitModelYear + luckyNumber)%66+1;
 						  PowerBallArray[3] = 42;
-						  PowerBallArray[4] = (someStringFive + someStringNine)%66+1;
-						  PowerBallArray[5] = (someStringEight*anArray[2])%66+1;
+						  PowerBallArray[4] = (ageOfPet + twoDigitModelYear)%66+1;
+						  PowerBallArray[5] = (jerseyNumber*anArray[2])%66+1;
 						  System.out.println("Lottery Numbers" + ": " + PowerBallArray[0] + ", " + PowerBallArray[1] + ", " + PowerBallArray[2] + ", " + PowerBallArray[3] + ", " + PowerBallArray[4] + "  Magic Ball: " + PowerBallArray[5]);
 						  System.out.println("Would you like to generate another set of numbers? ");
-						  String someStringTwelve = scanner.nextLine();
-						  if(someStringTwelve.equals(s) || someStringTwelve.equals(b)) {
+						  String generateAnotherSet = scanner.nextLine();
+						  if(generateAnotherSet.equals(s) || generateAnotherSet.equals(b)) {
 							  continue;
 						  }else {
 							  System.out.println("Thank you for participating.");
